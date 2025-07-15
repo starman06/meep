82a17997bcb80daea474f6885c31e30440bdd079
starman06 <pophamboone06@gmail.com>
2025-07-15 14:40:44 -0400

    Create meep.js

---
+++ meep.js
@@ -0,0 +1,10 @@
+function play() {
+    const audio = new Audio("filename.wav");
+    audio.play().catch(error => {
+        console.error("Error playing sound:", error);
+    });
+}
+
+function meep() {
+alert("MEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEP!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+}
